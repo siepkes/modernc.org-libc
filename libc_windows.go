4ac04d4b5cc0ae79751806372ed6c7cc2e5f3e7b
Jan Mercl <0xjnml@gmail.com>
2021-11-14 11:08:55 +0100

    windows: fix build on Go 1.16
    Updates sqlite#76
    
    commit 52bafb522078dd7082bf3727c84d05d1a2685842
    Author: Jan Mercl <0xjnml@gmail.com>
    Date:   Sun Nov 14 11:08:38 2021 +0100
    
        OK

--- libc_windows.go
+++ libc_windows.go
@@ -5424,5 +5424,9 @@ type __timeb64 struct {
 func X_ftime64(t *TLS, timeptr uintptr) {
 	tm := gotime.Now()
 	(*__timeb64)(unsafe.Pointer(timeptr)).time = types.X__time64_t(tm.Unix())
-	(*__timeb64)(unsafe.Pointer(timeptr)).millitm = uint32(tm.UnixMilli() % 1000)
+
+	//TODO When Go 1.16 is no more supported
+	// (*__timeb64)(unsafe.Pointer(timeptr)).millitm = uint32(tm.UnixMilli() % 1000)
+
+	(*__timeb64)(unsafe.Pointer(timeptr)).millitm = uint32(int64(tm.Nanosecond()) / 1e6)
 }
