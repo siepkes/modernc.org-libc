9b2cd02f2029ec2617723fabbe8ceb7b9d15b3e1
Roman Khafizianov <requilence@gmail.com>
2025-03-12 19:46:51 +0100

    rename mem_counters file

--- mem_expvar.go
+++ mem_expvar.go
@@ -5,7 +5,7 @@ package libc
 import "expvar"
 
 func init() {
-	// make sure to build with -tags=memory.counters to have the actual data
+	// make sure to build with -tags=memory.counters to have the actual data accumulated in memory allocator
 	expvar.Publish("memory.allocator", expvar.Func(func() interface{} {
 		return MemStat()
 	}))
