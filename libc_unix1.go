aca4954881254644e00867636d0d777c1cece810
Jasper Siepkes <siepkes@serviceplanet.nl>
2025-04-10 14:13:37 +0200

    Added illumos compatibility fixes.

--- libc_unix1.go
+++ libc_unix1.go
@@ -7,9 +7,13 @@
 package libc // import "modernc.org/libc"
 
 import (
+	"unsafe"
+
 	"golang.org/x/sys/unix"
 
+	"modernc.org/libc/errno"
 	"modernc.org/libc/sys/types"
+	ctime "modernc.org/libc/time"
 )
 
 // ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
@@ -24,4 +28,19 @@ func Xrecvmsg(t *TLS, sockfd int32, msg uintptr, flags int32) types.Ssize_t {
 	}
 
 	return types.Ssize_t(n)
+}
+
+func Xgmtime_r(tls *TLS, t uintptr, tm uintptr) (r uintptr) {
+	if __ccgo_strace {
+		trc("tls=%v t=%v tm=%v, (%v:)", tls, t, tm, origin(2))
+		defer func() { trc("-> %v", r) }()
+	}
+	if x___secs_to_tm(tls, int64(*(*time_t)(unsafe.Pointer(t))), tm) < 0 {
+		*(*int32)(unsafe.Pointer(X__errno_location(tls))) = int32(errno.EOVERFLOW)
+		return uintptr(0)
+	}
+	(*ctime.Tm)(unsafe.Pointer(tm)).Ftm_isdst = 0
+	(*ctime.Tm)(unsafe.Pointer(tm)).Ftm_gmtoff = 0
+	(*ctime.Tm)(unsafe.Pointer(tm)).Ftm_zone = uintptr(unsafe.Pointer(&x___utc))
+	return tm
 }
--- libc_unix2.go
+++ libc_unix2.go
@@ -7,7 +7,11 @@
 package libc // import "modernc.org/libc"
 
 import (
+	"unsafe"
+
+	"modernc.org/libc/errno"
 	"modernc.org/libc/sys/types"
+	ctime "modernc.org/libc/time"
 )
 
 // ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
@@ -23,4 +27,17 @@ func Xrecvmsg(t *TLS, sockfd int32, msg uintptr, flags int32) types.Ssize_t {
 	//	}
 	//
 	// return types.Ssize_t(n)
+}
+
+func Xgmtime_r(tls *TLS, t uintptr, tm uintptr) (r uintptr) {
+	if __ccgo_strace {
+		trc("tls=%v t=%v tm=%v, (%v:)", tls, t, tm, origin(2))
+		defer func() { trc("-> %v", r) }()
+	}
+	if x___secs_to_tm(tls, int64(*(*time_t)(unsafe.Pointer(t))), tm) < 0 {
+		*(*int32)(unsafe.Pointer(X__errno_location(tls))) = int32(errno.EOVERFLOW)
+		return uintptr(0)
+	}
+	(*ctime.Tm)(unsafe.Pointer(tm)).Ftm_isdst = 0
+	return tm
 }
